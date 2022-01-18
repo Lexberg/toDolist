e1ebb33cd13b168706eaf994ddc79b200004c757
Martin Privat <martinPrivat@Martins-MacBook-Pro-3.local>
2022-01-18 18:18:39 +0100

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList } from 'react-native';
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
+  const [courseGoals, setCourseGoals] = useState([]);
+  const [isAddMode, setIsAddMode] = useState(false);
+
+
+  const addGoalHandler = goalTitle => {
+    setCourseGoals(currentGoals => [...currentGoals, {id: Math.random().toString(), value: goalTitle}
+    ]);
+    setIsAddMode(false);
+  };
+
+  const removeGoalHandler = goalId => {
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter((goal) => goal.id !== goalId);
+      
+    });
+  };
+
+  const cancelInput = () => {
+    setIsAddMode(false);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <Button title="Add new Goal" onPress={() => setIsAddMode(true)}/>
+      <GoalInput 
+      visible={isAddMode} 
+      onAddGoal={addGoalHandler} 
+      onCancel={cancelInput}/>
+        <FlatList 
+        keyExtractor={(item, index) => item.id}
+        data={courseGoals} 
+        renderItem={itemData => <GoalItem id={itemData.item.id} onDelete={removeGoalHandler} title={itemData.item.value} />}
+          />
+          
+      {/* <View style={{ padding: 50, flexDirection: 'row'}}>
+       <View 
+         style={{
+          backgroundColor:'red',
+          width: 100,
+          height: 100,
+          justifyContent: 'center',
+          alignItems: 'center'
+      }}>
+          <Text>1</Text>
+       </View>
+       <View 
+         style={{
+           backgroundColor:'blue',
+           width: 100,
+           height: 100,
+           justifyContent: 'center',
+           alignItems: 'center'
+       }}>
+           <Text>2</Text>
+       </View>
+       <View 
+        style={{
+          backgroundColor:'green',
+          width: 100,
+          height: 100,
+          justifyContent: 'center',
+          alignItems: 'center'
+      }}>
+          <Text>3</Text>
+      </View>
+       </View> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 65
   },
 });
